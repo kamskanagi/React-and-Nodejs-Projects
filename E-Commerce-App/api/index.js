3d3b7f7629cd301d75ea21eaba89f2fbfc0610df
Kams Kanagi <karamo.kanagi@dodoker.com>
2023-02-13 23:41:47 +0800

    added stripe payment

--- E-Commerce-App/api/index.js
+++ E-Commerce-App/api/index.js
@@ -3,8 +3,12 @@ const mongoose = require("mongoose")
 const app = express()
 //const cors = require("cors");
 const dotenv = require('dotenv')
+const cors = require("cors")
 //dontenv.config()
 app.use(express.json())
+const stripeRouter = require("./src/routes/stripeRoute")
+const cartRouter = require("./src/routes/cartRoute")
+const orderRouter = require("./src/routes/orderRoute")
 const userRouter = require("./src/routes/userRoute")
 const productRouter = require("./src/routes/productRoute")
 const authRouter = require("./src/routes/auth")
@@ -21,9 +25,13 @@ mongoose
         console.log(err)
     })
 
+app.use(cors())
+app.use("/api/carts", cartRouter)
+app.use("/api/orders", orderRouter)
 app.use("/api/products", productRouter)
 app.use("/api/users", userRouter)
 app.use("/api/auth", authRouter)
+app.use("/api/checkout", stripeRouter)
 
 // app.get("/api/test", ()=>{
 //     console.log("test is successful")
